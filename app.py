7984080d5c2aae44d7cfe3155e3a5d799b673539
creekygreen <creekygreen@gmail.com>
2024-11-24 21:33:34 +0800

    integrated flask in frontend.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__,
 
 pages = [{
         "page_url": "dashboard",
-        "age_icon": "dashboard",
+        "page_icon": "dashboard",
         "page_name": "Dashboard"
     },
     {
@@ -17,27 +17,35 @@ pages = [{
         "page_name": "Balance Training"
     },
     {
-        "page_url": "aerobic",
+        "page_url": "aerobic_activity",
         "page_icon": "receipt_long",
         "page_name": "Aerobic Activity"
     },
     {
-        "page_url": "measure_recovery",
+        "page_url": "recovery_time",
         "page_icon": "view_in_ar",
-        "page_name": "Measure Recovery Time"
+        "page_name": "Recovery Time"
     }]
 
 @app.route('/')
 def home():
-    return render_template("dashboard.html", page=pages)
+    return render_template("dashboard.html", pages=pages)
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html', page=pages)
+    return render_template('dashboard.html', pages=pages)
 
 @app.route('/balance_training')
 def balance_training():
-    return render_template('balance_training.html', page=pages)
+    return render_template('balance_training.html', pages=pages)
+
+@app.route('/aerobic_activity')
+def aerobic_activity():
+    return render_template('aerobic_activity.html', pages=pages)
+
+@app.route('/measure_recovery')
+def recovery_time():
+    return render_template('recovery_time.html', pages=pages)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
