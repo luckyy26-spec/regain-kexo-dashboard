4bc60aab291b315c686bb2a2067461101318d573
greenman <89953760+creekygreen@users.noreply.github.com>
2024-11-25 22:21:58 +0800

    edited out rasp only code in app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+# from flask_socketio import SocketIO
+# from knee_data import get_knee_data
 
 app = Flask(__name__, 
             template_folder='pages', 
             static_folder='assets',
             static_url_path='/assets'
             )
+
+# socketio = SocketIO(app)
 
 pages = [{
         "page_url": "dashboard",
@@ -48,3 +52,28 @@ def recovery_time():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+    
+'''
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('start_data_stream')
+def stream_knee_data():
+    while True:
+        # try to catch exception in knee_data
+        try:
+            data = get_knee_data()
+            if data:
+                socketio.emit('knee_data', data)
+            else:
+                print("No data to emit")
+        except Exception as e:
+            print(f"Error in stream_knee_data: {e}")
+        socketio.sleep(0.1)
+
+if __name__ == "__main__":
+    billy_host = '192.168.221.24' # host=billy_host
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True) # allow_unsafe_werkzeug=True in order to run on thonny shell
+'''
+
