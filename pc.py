7e15c80d97fe272b8e31a895fd20667af5cf4ae0
greenman <89953760+creekygreen@users.noreply.github.com>
2024-11-29 13:33:13 +0800

    added pc .py file

--- pc.py
+++ pc.py
@@ -1,15 +1,10 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
-from knee_data import get_knee_data
-
 
 app = Flask(__name__, 
             template_folder='pages', 
             static_folder='assets',
             static_url_path='/assets'
             )
-
-socketio = SocketIO(app)
 
 pages = [{
         "page_url": "dashboard",
@@ -51,31 +46,5 @@ def aerobic_activity():
 def recovery_time():
     return render_template('recovery_time.html', pages=pages, exercises=exercises)
 
-@socketio.on('connect')
-def handle_connect():
-    print('Client connected')
-    
-@socketio.on('disconnect')
-def handle_disconnect():
-    print('Client disconnected')
-    
-@socketio.on('start_data_stream')
-def stream_knee_data():
-    while True:
-        # try to catch exception in knee_data
-        try:
-            data = get_knee_data()
-            if data:
-                socketio.emit('knee_data', data)
-            else:
-                socketio.emit('knee_data', {"error": "No data available"})
-                print("No data to emit")
-        except Exception as e:
-            print(f"Error in stream_knee_data: {e}")
-            socketio.emit('knee_data', {"error": f"Error: {str(e)}"})
-        socketio.sleep(0.1)
-
 if __name__ == "__main__":
-    billy_host = '192.168.221.24' # host=billy_host
-    eye_foam = '172.20.10.7' # host=eye_foam
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True, host=billy_host) # allow_unsafe_werkzeug=True in order to run on thonny shell
+    app.run(debug=True) 
